5a8971eca8d9ca16fb0ca72318a774d251ad190b
Hardeep Singh <hardeepphp@yahoo.com>
2017-08-17 16:34:17 +0530

    Fetch Products with FlatList

---
+++ nativeShop/pages/products2.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import { AppRegistry,View,Text,
+StyleSheet,FlatList,Image,StatusBar } from 'react-native';
+
+export default class products extends Component{
+			
+	static navigationOptions = {
+    title: 'Products2',	
+	};
+	
+	state={
+		data:[]
+	};
+	
+	
+	fetchData = async() =>{
+		const { params } = this.props.navigation.state;
+		const response =  await fetch('http://hardeepcoder.com/laravel/easyshop/api/products/' + params.id);
+		
+		const products = await response.json(); // products have array data
+		this.setState({data: products}); // filled data with dynamic array
+		
+	};
+	
+	componentDidMount(){
+		this.fetchData();
+	}
+	
+	
+	render(){
+		
+		 const { params } = this.props.navigation.state;
+		return(
+		 <View style={styles.container}>	
+		
+		<Text style={styles.pageName}>{params.cat}</Text>
+		  
+		  <FlatList
+		  data={this.state.data}
+		  keyExtractor={(x,i) => i}
+		  renderItem={({item})=>
+		  <View style={styles.productBox}>
+		  
+		  <Image style={{height:250, width:'100%'}}
+		  source={{uri:item.pro_img}}
+		  />
+		   <Text style={styles.price}>{item.pro_price}</Text>
+		  <Text style={styles.proName}>{item.pro_name}</Text>
+		 
+		  
+		  </View>
+			  
+		  }
+		  
+		  />
+	
+		
+		</View>
+			
+		);
+	}
+	
+		
+	
+}
+const styles = StyleSheet.create({
+	container:{
+		 flex: 1,
+        flexDirection: 'column',
+		justifyContent: 'center',
+       
+	},
+	pageName:{
+		margin:10,fontWeight:'bold',
+		color:'#000', textAlign:'center'
+	},
+	productBox:{
+		padding:5,margin:10,borderColor:'orange',borderBottomWidth:1
+	},
+	price:{
+		padding:5, color:'orange',fontWeight:'bold',textAlign:'center'
+	},
+	proName:{
+		padding:5,color:'blue',textAlign:'center'
+	}
+})
+AppRegistry.registerComponent('products', () => products);
