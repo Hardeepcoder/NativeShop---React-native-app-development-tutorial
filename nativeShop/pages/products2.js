1e47d5083fbfbb724870f9259a5bba8e48fe8294
Hardeep Singh <hardeepphp@yahoo.com>
2017-08-25 10:51:15 +0530

    Header HomePage Button

--- nativeShop/pages/products2.js
+++ nativeShop/pages/products2.js
@@ -1,74 +1,59 @@
 import React, { Component } from 'react';
 import { AppRegistry,View,Text,
-StyleSheet,FlatList,Image,StatusBar } from 'react-native';
+StyleSheet,FlatList,Image,StatusBar,TouchableOpacity,Button } from 'react-native';
 
 export default class products extends Component{
-			
-	static navigationOptions = {
-    title: 'Products2',	
-	};
+	static navigationOptions= ({navigation}) =>({
+		  title: 'Products',			
+		headerRight:<TouchableOpacity onPress={() => navigation.navigate("Home")}
+		style={{backgroundColor:'orange', margin:10,padding:10}}>
+		<Text style={{color:'#fff'}}>Home</Text></TouchableOpacity>
+	});
+			  
 	
 	state={
 		data:[]
 	};
-	
-	
 	fetchData = async() =>{
 		const { params } = this.props.navigation.state;
 		const response =  await fetch('http://hardeepcoder.com/laravel/easyshop/api/products/' + params.id);
-		
 		const products = await response.json(); // products have array data
 		this.setState({data: products}); // filled data with dynamic array
-		
 	};
-	
 	componentDidMount(){
 		this.fetchData();
 	}
-	
-	
 	render(){
-		
+
 		 const { params } = this.props.navigation.state;
 		return(
-		 <View style={styles.container}>	
-		
+		 <View style={styles.container}>
 		<Text style={styles.pageName}>{params.cat}</Text>
-		  
 		  <FlatList
 		  data={this.state.data}
 		  keyExtractor={(x,i) => i}
 		  renderItem={({item})=>
 		  <View style={styles.productBox}>
-		  
-		  <Image style={{height:250, width:'100%'}}
-		  source={{uri:item.pro_img}}
-		  />
+		  <Image style={{height:250, width:'100%'}} source={{uri:item.pro_img}} />
 		   <Text style={styles.price}>{item.pro_price}</Text>
-		  <Text style={styles.proName}>{item.pro_name}</Text>
-		 
-		  
+		  <Text style={styles.proName}>{item.pro_name}</Text>		  	
 		  </View>
-			  
 		  }
-		  
 		  />
 	
-		
 		</View>
-			
 		);
 	}
-	
-		
-	
+
+
+
 }
 const styles = StyleSheet.create({
 	container:{
 		 flex: 1,
         flexDirection: 'column',
 		justifyContent: 'center',
-       
+
 	},
 	pageName:{
 		margin:10,fontWeight:'bold',
